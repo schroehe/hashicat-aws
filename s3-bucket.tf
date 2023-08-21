5ae7a44a8f221043d4ed287f56824e9aa7b0b5fd
HS <HS>
2023-08-21 10:01:51 +0000

    add bucket

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3-bucket" {
   source  = "app.terraform.io/schroehe-aws/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket_prefix = "hs"
+  bucket_prefix = "hs-"
   acl    = "private"
 
   control_object_ownership = true
